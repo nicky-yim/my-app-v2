e2572913fc4e56e366f62dd984727c4f89ecae9a
Nicky Yim <nicky.yim@outlook.com>
2021-04-01 22:20:01 -0400

    more eslint configs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import * as React from 'react';
+import { Link } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
 const IndexPage = () => (
   <Layout>
@@ -15,15 +15,16 @@ const IndexPage = () => (
       src="../images/gatsby-astronaut.png"
       width={300}
       quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
+      formats={['AUTO', 'WEBP', 'AVIF']}
       alt="A Gatsby astronaut"
       style={{ marginBottom: `1.45rem` }}
     />
     <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/page-2/">Go to page 2</Link>
+      <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </p>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
