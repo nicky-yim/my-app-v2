1cdedb6194d57ba0ffb15a1431ce72dada2947d4
Nicky Yim <nicky-yim@users.noreply.github.com>
2021-09-05 03:40:28 -0400

    making it plain simple...

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,12 @@ import Layout from '../components/layout';
 import Hero from '../components/hero';
 import Portfolio from '../components/portfolio';
 import About from '../components/about';
-import Contact from '../components/contact';
 
 const IndexPage = () => (
   <Layout>
     <Hero />
     <Portfolio />
     <About />
-    <Contact />
   </Layout>
 );
 
